3af2bf0f05b0d547c88e1d3bbdbdb84227ab56f9
KLEEEEEER <gonzalx_747@mail.ru>
2021-02-01 13:11:05 +0900

    Added universalHidePlace and some paintings

---
+++ Assets/__Scripts/Interactables/HidePlaces/UniversalHidePlace.cs
@@ -0,0 +1,34 @@
+﻿using HeyEscape.Core.Player.FSM;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HeyEscape.Interactables.HidePlaces
+{
+    public class UniversalHidePlace : MonoBehaviour, IHidePlace
+    {
+        [SerializeField] private HidePlaceInfoSO hidePlaceInfoSO;
+        [SerializeField] private bool isOpened = true;
+
+        public HidePlaceInfoSO GetHidePlaceInfo()
+        {
+            hidePlaceInfoSO.transform = transform.position;
+            return hidePlaceInfoSO;
+        }
+
+        public bool IsAccessible()
+        {
+            return isOpened;
+        }
+
+        public void OnHide(PlayerFSM player)
+        {
+
+        }
+
+        public void OnUnhide(PlayerFSM player)
+        {
+
+        }
+    }
+}
